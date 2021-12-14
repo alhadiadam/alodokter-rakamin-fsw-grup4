1d1adeeec9178246b614114f8b66c321c4b87a6e
alhadiadam <alhadiadam13@gmail.com>
2021-12-14 23:18:53 +0700

    production.rb changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   Rails.application.routes.default_url_options[:host] = 'https://alodokter-rakamin-fsw-grup4.herokuapp.com'
-  config.action_mailer.default_url_options = { host: 'https://alodokter-rakamin-fsw-grup4.herokuapp.com', port: 587 }
+  config.action_mailer.default_url_options = { :host => 'https://alodokter-rakamin-fsw-grup4.herokuapp.com', port: 587 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :sendmail
