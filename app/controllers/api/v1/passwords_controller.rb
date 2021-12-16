b04304be497e49ef910895022cd6da78a68646b2
alhadiadam <alhadiadam13@gmail.com>
2021-12-16 12:58:58 +0700

    fix passwords

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -35,7 +35,7 @@ module Api
             token = params[:token].to_s
     
            @user = User.find_by(password_reset_token: token)
-           if @user.reset(password_params)
+           if @user.update(password_params)
             render json: {
                     status: 'ok',
                     message:'Successfully Reset Password!'
